5d08722c5fe5159135f4134ad8de7c8abb634f42
Shivkumar-Raghuwanshi <shivkumarraghuwanshi56@gmail.com>
2024-01-13 08:39:36 +0530

    updated

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -21,13 +21,13 @@ const LoginPage = () => {
           {<LoginForm />}
           <div className="flex flex-row items-center gap-4 mt-8 justify-center">
             <div
-              onClick={() => signIn("google", { callbackUrl: "/home" })}
+              onClick={() => signIn("google", { callbackUrl: "/" })}
               className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
             >
               <FcGoogle size={32} />
             </div>
             <div
-              onClick={() => signIn("github", { callbackUrl: "/home" })}
+              onClick={() => signIn("github", { callbackUrl: "/" })}
               className="w-10 h-10 bg-black rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
             >
               <FaGithub size={40} />
